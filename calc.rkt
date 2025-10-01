b3c491a712ad87c9f4dc5a345f00572df6e60224
Mikael Rehman <mikaelr112@gmail.com>
2025-09-30 21:38:44 -0500

    Add interactive mode with input prompt and evaluation loop

--- calc.rkt
+++ calc.rkt
@@ -1,3 +1,10 @@
 #lang racket
 
 (define interactive? (not (member "-b" (current-command-line-arguments))))
+
+(define (process-expression)
+    (display "Enter an expression: ")
+    (define user-input(read))
+    (display "You entered: ")
+    (display user-input)
+    (newline))
